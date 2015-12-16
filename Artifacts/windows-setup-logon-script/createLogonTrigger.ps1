ba695487bed73e76cd39dd160bfe87f9e2a10c0f
Mark Mydland <markmyd@microsoft.com>
2015-12-16 22:18:44 +0000

    create an artifact that schedules a startup task

---
+++ Artifacts/windows-setup-logon-script/createLogonTrigger.ps1
@@ -0,0 +1,9 @@
+﻿Param(
+$Action,
+$Name)
+
+$taskAction = New-ScheduledTaskAction –Execute $Action
+$taskTrigger = New-ScheduledTaskTrigger -AtLogOn
+$scheduledTask = New-ScheduledTask -Action $taskAction -Trigger $taskTrigger
+Register-ScheduledTask $Name -InputObject $scheduledTask
+
